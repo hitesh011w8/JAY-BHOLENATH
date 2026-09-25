207a836cf870195017aba202d91fc7ae74db93e0
hitesh011w8 <hiteshsen935173@gmail.com>
2026-09-25 14:36:45 +0530

    Fix indentation for drive URL check

--- main.py
+++ main.py
@@ -924,7 +924,7 @@ async def txt_handler(bot: Client, m: Message):
                 ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1} .mp3`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
                 cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** `{name1} .html`\n<blockquote><b>Batch Name :</b> {b_name}</blockquote>\n\n**Extracted by➤**{CR}\n'
                   
-                 if "drive" in url:
+                if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=channel_id,document=ka, caption=cc1)
